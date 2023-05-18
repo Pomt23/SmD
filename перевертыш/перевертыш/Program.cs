ddbf1b949870e7598cddb9b2f14b3c0101631eb1
Pomt23 <126857570+Pomt23@users.noreply.github.com>
2023-05-18 13:25:50 +0300

    Add files via upload

---
+++ перевертыш/перевертыш/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace перевертыш
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите строку");
+            string text = Console.ReadLine().Trim();
+
+            char[] letters = text.ToCharArray();
+            Array.Reverse(letters);
+
+            text = String.Concat(letters);
+            Console.WriteLine(text);
+            Console.ReadKey();
+
+        }
+    }
+}
+
