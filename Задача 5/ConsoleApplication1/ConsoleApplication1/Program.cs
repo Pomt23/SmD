7c44084ce43bd5cf7f0ce72c9ab0552868435072
Pomt23 <126857570+Pomt23@users.noreply.github.com>
2023-05-18 13:23:59 +0300

    Add files via upload

---
+++ Задача 5/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace C5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число:  ");
+            int q = int.Parse(Console.ReadLine());
+            int z = q * q * q *q * q * q;
+            Console.WriteLine("Введенное число в шестой степени будет:" + z);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 6/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace C6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите сторону квадрата:  ");
+            int q = int.Parse(Console.ReadLine());
+            int z = q * 4;
+            Console.WriteLine("Периметр квадрата равен: " + z);
+            Console.ReadKey();
+        }
+    }
+}
